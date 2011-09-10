c1b8db66519a8ab1bac230e6c38b094cef064273
Brendon Murphy <xternal1+github@gmail.com>
2011-09-09 23:19:27 -0700

    changing autoload path info.

--- lib/cinch/plugins/last_seen.rb
+++ lib/cinch/plugins/last_seen.rb
@@ -3,8 +3,8 @@ require 'cinch'
 module Cinch
   module Plugins
     module LastSeen
-      autoload :Base, File.expand_path('./lib/cinch/plugins/last_seen/base.rb')
-      autoload :RedisStorage, File.expand_path('./lib/cinch/plugins/last_seen/storage/redis_storage.rb')
+      autoload :Base, File.expand_path('../last_seen/base.rb', __FILE__)
+      autoload :RedisStorage, File.expand_path('../last_seen/storage/redis_storage.rb', __FILE__)
     end
   end
 end
